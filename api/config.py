5fd75f0d894e14c2d89dca9f83d1c4685d559e45
algmoraes01 <alg.moraes001@gmail.com>
2025-11-04 21:49:05 -0300

    FIX: Corrige FileNotFoundError usando ROOT_DIR explicito no config.py

--- api/config.py
+++ api/config.py
@@ -1,13 +1,15 @@
 import os
 from dotenv import load_dotenv
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 load_dotenv()
+
+API_DIR = os.path.dirname(os.path.abspath(__file__)) 
+
+ROOT_DIR = os.path.dirname(API_DIR) 
 
 class Config:
     DEBUG = True
     SECRET_KEY = os.getenv("SECRET_KEY", "fiap-secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
     
-    DATA_PATH = os.path.join(BASE_DIR, "..", "data", "books.csv")
+    DATA_PATH = os.path.join(ROOT_DIR, "data", "books.csv")
