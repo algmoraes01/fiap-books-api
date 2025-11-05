234cae7220332c36703e4c362289ae748cc9a588
algmoraes01 <alg.moraes001@gmail.com>
2025-11-04 21:04:49 -0300

    Corrige execução Flask para modo produção no Railway

--- api/app.py
+++ api/app.py
@@ -304,5 +304,4 @@ api.add_namespace(ns)
 app = app
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
