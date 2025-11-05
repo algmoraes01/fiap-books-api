728fc79d2225aadf993c0f2c896b92404d27735f
algmoraes01 <alg.moraes001@gmail.com>
2025-11-04 21:39:48 -0300

    FIX: Torna o DATA_PATH para books.csv absoluto no config.py

--- api/config.py
+++ api/config.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 load_dotenv()
 
 class Config:
     DEBUG = True
     SECRET_KEY = os.getenv("SECRET_KEY", "fiap-secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
-    DATA_PATH = os.getenv("DATA_PATH", "data/books.csv")
+    
+    DATA_PATH = os.path.join(BASE_DIR, "..", "data", "books.csv")
